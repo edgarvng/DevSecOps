81700c2c3a540730f0e13a9ad2734f82e5ba097c
EdgarVenegas <edgar.vnegas@gmail.com>
2023-11-06 14:29:32 -0500

    Se sube el pipeline gestion 5

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -18,7 +18,7 @@ output "My_ip"{
 }
 
 resource "aws_security_group" "my_ec2" {
-  name        = "aws-security-group-2"
+  name        = "aws-security-group-4"
   description = "Security Group"
   vpc_id = "vpc-0dc979631fedb2186"
  
