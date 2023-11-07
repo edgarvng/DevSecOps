49ec4e9d6f4026360a1b694e814a1f831ba04d68
EdgarVenegas <edgar.vnegas@gmail.com>
2023-11-07 10:28:42 -0500

    Se sube el pipeline final de gestion y devsecops

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -18,7 +18,7 @@ output "My_ip"{
 }
 
 resource "aws_security_group" "my_ec2" {
-  name        = "aws-security-group-vfin1"
+  name        = "aws-security-group-v1fin"
   description = "Security Group"
   vpc_id = "vpc-0dc979631fedb2186"
  
